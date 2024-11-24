56e30fb595c2d950fca30865cf4695e32493fda5
Ben Eberle <eberleben15@gmail.com>
2024-11-24 11:28:11 -0700

    initilizer setup

---
+++ src/main/java/com/example/usermanagement/UsermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring-user-management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class spring-user-managementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(spring-user-managementApplication.class, args);
+	}
+
+}
